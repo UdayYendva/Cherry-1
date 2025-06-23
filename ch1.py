fe50da36d655654ff2b9cc97309d11a55b55398a
Uday Yendava <uyendava@uyendava-thinkpadp1gen4i.punetw6.csb>
2025-06-23 15:54:00 +0530

    CH1

---
+++ ch1.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"Sum: {addition}, Difference: {subtraction}, Product: {multiplication}, Quotient: {division}")
